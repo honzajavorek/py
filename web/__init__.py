3c849d18350874f0c8daa7fdec231c8d3e704275
Honza Javorek <mail@honzajavorek.cz>
2019-02-02 20:10:17 +0100

    make sure the app fails fast on production

--- web/__init__.py
+++ web/__init__.py
@@ -22,9 +22,12 @@ def events():
     try:
         events = json.loads(data_path.read_text())
     except FileNotFoundError:
-        logging.warning((
-            "There is no data in '%s'. "
-            "Run 'pipenv run data.events' and retry the request to see content"
-        ), data_path)
-        events = []
+        if app.debug:
+            logging.warning((
+                "There is no data in '%s'. "
+                "Run 'pipenv run data.events' and retry the request to see content"
+            ), data_path)
+            events = []
+        else:
+            raise
     return render_template('events.html', events=events)
