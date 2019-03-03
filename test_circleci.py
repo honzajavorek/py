ad2b4235c485b406ea4f046ef4a69a2dd13d1eae
Honza Javorek <mail@honzajavorek.cz>
2019-03-03 22:57:29 +0100

    fix CircleCI config and its tests

--- test_circleci.py
+++ test_circleci.py
@@ -4,8 +4,13 @@ import pytest
 import yaml
 
 
-BUILDERS_PATH = Path(__file__).parent / 'data'
-BUILDERS = [b for b in BUILDERS_PATH.iterdir() if b.is_dir()]
+PROJECT_PATH = Path(__file__).parent
+
+PAGES_PATH = PROJECT_PATH / 'pythoncz' / 'pages'
+PAGE_BUILDERS_NAMES = [
+    item.name for item in PAGES_PATH.iterdir()
+    if item.is_dir() and (PAGES_PATH / item / '__main__.py').is_file()
+]
 
 
 @pytest.fixture
@@ -14,9 +19,9 @@ def config():
     return yaml.safe_load(path.read_text())
 
 
-@pytest.mark.parametrize('builder', BUILDERS)
-def test_ci_config_runs_all_builders(builder, config):
-    job_name = f'build_{builder.name}'
+@pytest.mark.parametrize('builder_name', PAGE_BUILDERS_NAMES)
+def test_ci_config_runs_all_page_builders(builder_name, config):
+    job_name = f'build_{builder_name}'
 
     assert job_name in config['jobs']
 
@@ -24,22 +29,25 @@ def test_ci_config_runs_all_builders(builder, config):
 
     assert job['executor'] == 'python'
     assert job['steps'][0] == {'attach_workspace': {'at': '~'}}
-    assert job['steps'][1] == {'run': f'pipenv run build {builder.name}'}
+    assert job['steps'][1] == {'run': f'pipenv run build {builder_name}'}
     assert job['steps'][2] == {
         'persist_to_workspace': {
             'root': '~',
-            'paths': [f'project/data/{builder.name}']
+            'paths': [f'project/pythoncz/pages/{builder_name}/*_data.json']
         }
     }
 
 
-def test_ci_config_fans_out_builders_in_workflow(config):
+def test_ci_config_fans_out_page_builders_in_workflow(config):
     default_workflow_jobs = config['workflows']['default_workflow']['jobs']
 
-    builders_job_names = [f'build_{builder.name}' for builder in BUILDERS]
+    builders_job_names = [
+        f'build_{builder_name}'
+        for builder_name in PAGE_BUILDERS_NAMES
+    ]
     builders_jobs = [
-        {f'build_{builder.name}': {'requires': ['install_and_test']}}
-        for builder in BUILDERS
+        {f'build_{builder_name}': {'requires': ['install_and_test']}}
+        for builder_name in PAGE_BUILDERS_NAMES
     ]
     builders_count = len(builders_job_names)
 
